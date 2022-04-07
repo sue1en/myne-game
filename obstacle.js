9a095dd245c77a76bd6fa93856c88ec4fd1dc350
Suelen dos Santos Batista <suelen.batista@al.infnet.edu.br>
2022-04-07 20:18:52 -0300

    add obstacle file

---
+++ obstacle.js
@@ -0,0 +1,16 @@
+function Obstacles(ctx, x, y, color){
+  this.width = 40;
+  this.height = 70;
+  this.x = x;
+  this.y = y;
+  this.color = color;
+  this.update = function(){
+    // ctx = document.getElementById("gameCanvas").getContext("2d");
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  };
+};
+
+export {
+  Obstacles
+}
